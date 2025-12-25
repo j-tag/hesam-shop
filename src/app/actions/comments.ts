3a31e752463ae552374cc1ba91890b16e9e01d00
jtag <hesamgholami@yahoo.com>
2025-12-25 17:24:18 +0330

    feat: add Zod validation for comment submissions and update sendComment logic

--- src/app/actions/comments.ts
+++ src/app/actions/comments.ts
@@ -1,13 +1,19 @@
 "use server"
 
-import { CommentForm, SavedCommentForm } from "@/app/db/types"
+import { SavedCommentForm } from "@/app/db/types"
 import { revalidatePath } from "next/cache"
 import { mongo } from "@/app/lib/mongo"
+import { CommentForm, CommentFormSchema } from "@/app/lib/schema"
 
 export async function sendComment(
   prevState: CommentForm | null,
   values: CommentForm,
 ) {
+  const validationResult = CommentFormSchema.safeParse(values)
+  if (!validationResult.success) {
+    return null
+  }
+
   mongo
     .collection<{
       username: string
@@ -17,7 +23,7 @@ export async function sendComment(
       { username: "test-username" },
       {
         $push: {
-          comments: { ...values, id: crypto.randomUUID() },
+          comments: { ...validationResult.data, id: crypto.randomUUID() },
         },
         $setOnInsert: { username: "test-username" },
       },
